37ddaa32f16725994fc9c549140985b17cbcee11
Kunal Dalwadi <kunaldalwadi@gmail.com>
2020-10-14 06:12:12 -0400

    [KD]First App with basic Title bar and body text

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 
+//main function automatically executed when app launches
 void main() {
   runApp(MyApp());
 }
 
+//class that presents the widget on the scaffold(screen)
+//very similar to class xyz extends activity
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     // throw UnimplementedError();
     return MaterialApp(
-      home: Text("Heyaaa"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Text("This is Body"),
+      ),
     );
   }
 }
