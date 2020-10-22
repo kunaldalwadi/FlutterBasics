76524397491277e0bda5ef1ae45e1d8f1de7317c
Kunal Dalwadi <kunaldalwadi@gmail.com>
2020-10-22 06:49:07 -0400

    [KD][FB-1] Added Text and Buttons, also worked with onPressed(onclick) attribute of a Button.

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,51 @@ void main() {
 //class that presents the widget on the scaffold(screen)
 //very similar to class xyz extends activity
 class MyApp extends StatelessWidget {
+
+  var qIndex = 0;
+
+  void doSomething() {
+    print("Clicked this Button !");
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     // throw UnimplementedError();
+
+    var questions = [
+      "What is your fav colour?",
+      "What is your fav animal?",
+      "What is your fav fruit?"
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        body: Text("This is Body"),
+        body: Column(
+          children: [
+            Text("Who are you?"),
+            Text(questions.elementAt(1)),
+            Text(questions[2]),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: doSomething,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: () => print("This is Lambda function"),
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: () {
+                //... something this function does
+                print("This is the 3rd Button");
+              },
+            )
+          ],
+        ),
       ),
     );
   }
